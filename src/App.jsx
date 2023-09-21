34f03f3ff8d493a9b94921c0a5ad1d16f3ba48a9
Iannacus <ian.rosasmal@gmail.com>
2023-09-20 19:02:32 -0600

    fix router & validate email view

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import { ThemeProvider } from "@mui/material";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import LoginPage from "./modules/auth/login/pages/LoginPage";
+import { RouterProvider } from "react-router-dom";
 import theme from "./theme/newTheme";
-import AuthLayout from "./modules/auth/AuthLayout";
-import RegisterPage from "./modules/auth/register/pages/RegisterPage";
-import ValidateEmail from "./modules/auth/validate-email/ValideteEmail";
-import Chat from "./modules/chat/pages/Chat";
-import Home from "./modules/home/pages/Home";
-import Conversation from "./modules/chat/components/Conversation";
-import ProtectedRoute from "./routes/ProtectedRoutes";
+import router from "./routes";
 
 function App() {
   return (
@@ -19,37 +12,3 @@ function App() {
 }
 
 export default App;
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/auth",
-    element: <AuthLayout />,
-    children: [
-      {
-        path: "login",
-        element: <LoginPage />,
-      },
-      {
-        path: "register",
-        element: <RegisterPage />,
-      },
-      {
-        path: "email-validation",
-        element: <ValidateEmail />,
-      },
-    ],
-  },
-  {
-    path: "/chats",
-    element: (
-      <ProtectedRoute>
-        <Chat />
-      </ProtectedRoute>
-    ),
-    children: [{ path: "conversation/:id", element: <Conversation /> }],
-  },
-]);
--- src/modules/auth/validate-email/ValideteEmail.jsx
+++ src/modules/auth/validate-email/ValideteEmail.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { useSearchParams } from "react-router-dom";
 import axios from "axios";
+import validate from "../../../assets/valid-email.png";
 
 function ValidateEmail() {
   const [valid, setValid] = useState("");
@@ -14,59 +15,81 @@ function ValidateEmail() {
 
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if (time <= 0) {
-  //     navigate("/auth/login");
-  //   }
-  //   const interval = setInterval(() => {
-  //     setTime(time - 1);
-  //   }, 1000);
-  //   return () => clearInterval(interval);
-  // }, [time]);
+  useEffect(() => {
+    let interval;
+    if (valid) {
+      if (time <= 0) {
+        navigate("/auth/login");
+      }
+      interval = setInterval(() => {
+        setTime(time - 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [time, valid]);
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "30px",
-        height: "100vh",
-      }}
-    >
-      <Typography variant="h3">Validando tu correo</Typography>
-      {completed && valid === 204 && (
-        <>
-          <Typography variant="h4" color="secondary">
-            El correo ha sido verificado
-          </Typography>
-          <Typography variant="body2" color="secondary">
-            Se esta redirigiendo a login {time} si no eres redirigido has click
-            <Typography
-              sx={{
-                "&:hover": {
-                  cursor: "pointer",
-                },
-              }}
-              component="span"
-              color="primary"
-              onClick={() => {
-                navigate("/auth/login");
-              }}
-            >
-              {" "}
-              aqui
+    <Grid container>
+      <Grid item xs={12} md={6}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "30px",
+            height: "100vh",
+          }}
+        >
+          <Typography variant="h3">Validando tu correo</Typography>
+          {completed && valid === 204 && (
+            <>
+              <Typography variant="h4" color="secondary">
+                El correo ha sido verificado
+              </Typography>
+              <Typography variant="body2" color="secondary">
+                Se esta redirigiendo a login {time} si no eres redirigido has
+                click
+                <Typography
+                  sx={{
+                    "&:hover": {
+                      cursor: "pointer",
+                    },
+                  }}
+                  component="span"
+                  color="primary"
+                  onClick={() => {
+                    navigate("/auth/login");
+                  }}
+                >
+                  {" "}
+                  aqui
+                </Typography>
+              </Typography>
+            </>
+          )}
+          {completed && valid !== 204 && (
+            <Typography variant="h4" color="secondary">
+              Ocurrio un error
             </Typography>
-          </Typography>
-        </>
-      )}
-      {completed && valid !== 204 && (
-        <Typography variant="h4" color="secondary">
-          Ocurrio un error
-        </Typography>
-      )}
-    </Box>
+          )}
+        </Box>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "30px",
+            height: "100vh",
+          }}
+        >
+          <img src={validate} alt="chat" />
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
